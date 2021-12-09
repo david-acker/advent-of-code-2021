29839d4683ba795ef987e6c39997c8f4efc67f4a
David Acker <davidacker3@gmail.com>
2021-12-08 22:20:15 -0500

    Clean up

--- solutions/day_8/seven-segment-search.clj
+++ solutions/day_8/seven-segment-search.clj
@@ -1,30 +1,25 @@
 (ns seven-segment-search
   (:require [clojure.string :as string]))
 
-(def file-name "C:\\Users\\david\\source\\repos\\advent-of-code-2021\\input\\day_8.txt")
-
+(def file-name "C:\\Users\\david\\source\\repos\\advent-of-code-2021\\input\\day_8_test.txt")
 (def input-data (string/split-lines (slurp file-name)))
 
-(defn extract-display-values [i]
-  (-> i
+;; Part One
+(defn extract-display-values [input]
+  (-> input
       (string/split #"\|")
       (nth 1)
       (string/trim)
       (string/split #" ")))
 
-(defn get-all-display-values [x]
-  (->> x
-       (map extract-display-values)
-       (flatten)))
-
 (defn get-part-one-result [input]
   (->> input
-       (get-all-display-values)
+       (map extract-display-values)
+       (flatten)
        (map count)
-       (map (fn [i] (.contains [2, 3, 4, 7] i)))
+       (map (fn [x] (.contains [2, 3, 4, 7] x)))
        (filter true?)
        (count)))
 
-;; Part One
 (def part-one-result (get-part-one-result input-data))
 (println part-one-result)
