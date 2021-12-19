74a4319500e7936d5602bddc3094e44e5b12c653
David Acker <davidacker3@gmail.com>
2021-12-18 23:26:12 -0500

    Add solution for day 16, part 2

--- solutions/day_16/packet_decoder.py
+++ solutions/day_16/packet_decoder.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 hex_values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 binary_values = ['0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111']
 
@@ -11,15 +13,39 @@ class Packet:
         self.sub_packets: list[Packet] = []
         self.value: int = None
 
+    def get_value(self):
+        if self.value is not None:
+            return self.value
+        
+        sub_packet_values = [sp.get_value() for sp in self.sub_packets]
+        
+        match self.type_id:
+            case 0:
+                return sum(sub_packet_values)
+            case 1:
+                return reduce(lambda x, y: x * y, sub_packet_values)
+            case 2:
+                return min(sub_packet_values)
+            case 3:
+                return max(sub_packet_values)
+            case 5:
+                return int(sub_packet_values[0] > sub_packet_values[1])
+            case 6:
+                return int(sub_packet_values[0] < sub_packet_values[1])
+            case 7:
+                return int(sub_packet_values[0] == sub_packet_values[1])
+            case _:
+                return 0
+
 class Transmission:
     def __init__(self, hex_transmission: str):
-        self.data: str = ''.join([hex_to_binary[x] for x in hex_transmission])
+        self.binary: str = ''.join([hex_to_binary[x] for x in hex_transmission])
         self.packets: list[Packet] = []
 
     def parse_packet(self, position: int) -> tuple[Packet, int]:
 
-        version = int(self.data[position:position + 3], 2)
-        type_id = int(self.data[position + 3:position + 6], 2)
+        version = int(self.binary[position:position + 3], 2)
+        type_id = int(self.binary[position + 3:position + 6], 2)
         position += 6
 
         packet = Packet(version, type_id)
@@ -28,7 +54,7 @@ class Transmission:
         if packet.type_id == 4:
             literal_value = ''
             while True:
-                group = self.data[position:position + 5]
+                group = self.binary[position:position + 5]
                 position += 5
 
                 literal_value += group[1:]
@@ -37,11 +63,11 @@ class Transmission:
                     packet.value = int(literal_value, 2) 
                     break
         else:
-            length_type_id = self.data[position]
+            length_type_id = self.binary[position]
             position += 1
 
             if length_type_id == '0':
-                length = int(self.data[position:position + 15], 2)
+                length = int(self.binary[position:position + 15], 2)
                 position += 15
 
                 packet_end = position + length
@@ -50,7 +76,7 @@ class Transmission:
                     sub_packet, position = self.parse_packet(position)
                     packet.sub_packets.append(sub_packet)
             else:
-                length = int(self.data[position:position + 11], 2)
+                length = int(self.binary[position:position + 11], 2)
                 position += 11
 
                 for x in range(length):
@@ -64,7 +90,11 @@ with open('C:/Users/david/source/repos/advent-of-code-2021/input/day_16.txt') as
 
 # Part One
 transmission = Transmission(hex_transmission)
-transmission.parse_packet(0)
+packet, _ = transmission.parse_packet(0)
 
 partOneResult = sum([p.version for p in transmission.packets])
 print(partOneResult)
+
+# Part Two
+partTwoResult = packet.get_value()
+print(partTwoResult)
